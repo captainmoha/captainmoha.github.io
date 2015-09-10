3e705eb6148be2c937df79edc8214c048de8a575
captainmoha <admiralmoha@gmail.com>
2015-09-10 13:37:24 +0200

    debugging javascript

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-$(document).ready(function(){
-  $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        $('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
-      }
-    }
-  });
+$(document).ready(function() {
+    $('a[href*=#]').click(function() {
+      console.log("Pathnames");
+      console.log(location.pathname);
+      console.log(this.pathname);
+      console.log("hostname");
+      console.log(location.hostname);
+      console.log(this.hostname);
+      console.log("-----------------------------------");
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body')
+                    .animate({
+                        scrollTop: targetOffset
+                    }, 1000);
+                return false;
+            }
+        }
+    });
 });

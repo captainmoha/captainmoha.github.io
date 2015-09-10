94133d0cbad391a99bb2594083f9ff101d6edc9c
captainmoha <admiralmoha@gmail.com>
2015-09-10 14:14:14 +0200

    change scrolling animation speed

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-$(document).ready(function() {
-    $('a[href*=#]').click(function() {
-      console.log("Pathnames");
-      console.log(location.pathname);
-      console.log(this.pathname);
-      console.log("hostname");
-      console.log(location.hostname);
-      console.log(this.hostname);
-      console.log("-----------------------------------");
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-          console.log("this.hash" + this.hash);
-            var $target = $(this.hash);
-            console.log("$target = $(this.hash)" + $target);
-            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
-            console.log(typeof($target) + " $target " + $target);
-            if ($target.length) {
-                var targetOffset = $target.offset().top;
-                $('html,body')
-                    .animate({
-                        scrollTop: targetOffset
-                    }, 1000);
-                return false;
-            }
-        }
-    });
+$(document).ready(function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 750);
+       return false;
+      }
+    }
+  });
 });
